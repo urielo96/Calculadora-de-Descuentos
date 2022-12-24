04914fee03f15e1ce09cde55dc23d793a8107a2f
Diego Uriel Alvarez Almanza <diegouriel96@hotmail.com>
2022-12-23 19:07:26 -0600

    Calculadora de Descuento Sencilla

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-const precio = document.querySelector('.pro')
-const descuento = document.querySelector('.desc')
-const cuadroResultado = document.querySelector('.resultado') 
-const button = document.querySelector('#botonCalcular')
+const inputPrice = document.querySelector('#price');
+const inputDiscount = document.querySelector('#discount');
+const outPut = document.querySelector('#result');
+const btn = document.querySelector('#calcular');
 
 
-button.addEventListener("click",calcDescuento);
+btn.addEventListener('click',CalcularPrecioConDescuento)
 
-function calcDescuento(event) {
-    event.preventDefault()
+function CalcularPrecioConDescuento() {
+    const price = Number(inputPrice.value);
+    const discount = Number(inputDiscount.value);
     
-    let precioPagar = ((precio.value * (100 - descuento.value))/100)
-    cuadroResultado.innerHTML = `El precio con descuento es de ${descuento.value} % de descuento es : ${precioPagar}`
+    console.log({price,discount});
+    
+    
+    if (!price || !discount){
+
+        outPut.innerText = 'Uuups Ocurrio un Error'
+        return
+    }
+    
+    if (discount > 100){
+
+        outPut.innerText = 'El descuento no puede ser mayor de 100'
+        return
+    }
+    
+
+    const newPrice = (price *  (100 - discount) / 100)
+
+    outPut.innerText = `El precio con descuento es de $ ${newPrice}`
+
 }
-
-
-
-
-
