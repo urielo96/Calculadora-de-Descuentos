92e6057f249bfba3d2981dff1568842acaf11d88
Diego Uriel Alvarez Almanza <diegouriel96@hotmail.com>
2022-12-26 12:55:57 -0600

    Comprobando si el cupon es valido

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
+const inputPrecio = document.querySelector('#inputPrecio')
 const inputCupon = document.querySelector('#inputCupon');
 const outPut = document.querySelector('#result');
 const btn = document.querySelector('#calcular');
 
+// Array con obejos de Ejemplo para aplicarles metodos
 
-const cupon = ['pedro','luis','juan'];
+const cupones = {
+    'navidad' : 30,
+    'diademuertos': 50,
+    'urielo': 100
+}
 
-btn.addEventListener('click',aplicarCupon)
 
 
-function aplicarCupon() {
+
+
+btn.addEventListener('click',calcularNuevoPrecio)
+
+ function calcularNuevoPrecio(){
     
-    result = cupon.includes(inputCupon.value) 
-    
-    if (result == true) {
-        alert('Tienes descuento')
-    } else {
-        alert('Introduzca un cupon valido')
+    const precio = Number(inputPrecio.value);
+    const cupon = inputCupon.value;
+
+    if (!precio || !cupon) {
+        outPut.innerText = 'Por favor rellene los 2 campos'
+        return
     }
 
+    let discount;
+
+    if (cupones[cupon]) {
+        discount = cupones[cupon]
+    } else {   
+        outPut.innerText = 'Introduzca un Cupon valido';
+        return
     }
+
+    console.log(discount);
+
+    const newPrice = (precio * (100 - discount) / 100);
+
+    outPut.innerText = `El nuevo descuento es de ${newPrice}`
+}
