d94d04e50506af05f5eff013a17ae119c8d96755
Diego Uriel Alvarez Almanza <diegouriel96@hotmail.com>
2022-12-27 15:20:58 -0600

    Hize una funcion que de dice si una lista es par o Impar

--- script.js
+++ script.js
@@ -5,33 +5,49 @@ const btnNumber = document.querySelector('#btnNumber')
 const btnCalc = document.querySelector('#btnCalc')
 
 
-const numbers = []
 
-btnNumber.addEventListener('click',addNumber);
-
-
-function addNumber() {
-    numbers.push(number.value)
-    salida.innerText = `Los numeros son ${numbers}`
-}
-
-btnCalc.addEventListener('click',calcAverage);
-
-function calcAverage() {
+function paroImpar(array) {
+    const numberElements = array.length
     
-    let acumulator = 0 ;
-
-    for (let i = 0; i < numbers.length; i++) {
-         let numberInArray = numbers[i];
-        
-        acumulator = acumulator + Number(numberInArray);
-        
+    if ((numberElements % 2) == 0) {
+        return console.log(`La lista es par con un total de ${numberElements} en total`);
+    } else {
+        return console.log(`La lista es impar con ${numberElements} elementos`);
     }
-
-    let average = acumulator / numbers.length 
-    finalResult.innerText = ` El promedio es ${average}`
-    
 }
+
+
+
+
+
+
+// const numbers = []
+
+// btnNumber.addEventListener('click',addNumber);
+
+
+// function addNumber() {
+//     numbers.push(number.value)
+//     salida.innerText = `Los numeros son ${numbers}`
+// }
+
+// btnCalc.addEventListener('click',calcAverage);
+
+// function calcAverage() {
+    
+//     let acumulator = 0 ;
+
+//     for (let i = 0; i < numbers.length; i++) {
+//          let numberInArray = numbers[i];
+        
+//         acumulator = acumulator + Number(numberInArray);
+        
+//     }
+
+//     let average = acumulator / numbers.length 
+//     finalResult.innerText = ` El promedio es ${average}`
+    
+// }
     
 
     
